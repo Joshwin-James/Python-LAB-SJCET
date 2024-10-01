f686eb44e301164c2f5a8f6fccb207ff43821c00
JOSHWIN JAMES <joshwinjames2028@cs.sjcetpalai.ac.in>
2024-10-01 15:23:57 +0530

    Document header Added

--- student_details.py
+++ student_details.py
@@ -1,3 +1,8 @@
+'''
+Created by Joshwin James
+Date : 1-10-2024
+For the details of student
+'''
 a=input("Enter The name of student :")
 b=int(input("Enter the Roll no :"))
 c=float(input("Enter the CGPA :"))
