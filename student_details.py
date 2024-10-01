537323305045a4543884c4177ffbc170b5cf0f8c
JOSHWIN JAMES <joshwinjames2028@cs.sjcetpalai.ac.in>
2024-10-01 14:54:17 +0530

    Add files via upload
    My First Commit..!!

---
+++ student_details.py
@@ -0,0 +1,8 @@
+a=input("Enter The name of student :")
+b=int(input("Enter the Roll no :"))
+c=float(input("Enter the CGPA :"))
+
+print()
+print("Name Of Student=",a)
+print("Roll No Of Student=",b)
+print("CGPA of student :",c*10,"%")
