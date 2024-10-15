0969b9cab752f64f85c8e0b5b95039cad1c52828
JOSHWIN JAMES <joshwinjames2028@cs.sjcetpalai.ac.in>
2024-10-15 14:06:35 +0530

    Add files via upload

---
+++ integer.py
@@ -0,0 +1,9 @@
+'''Author : Joshwin James
+Date : 15-10-2024
+To check the number is positive or negetive'''
+
+no=int(input("enter number"))
+if no>0:
+    print("The number is positive")
+else:
+    print("The number is negetive")
