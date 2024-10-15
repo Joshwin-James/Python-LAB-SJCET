402e71ba1a33b3ee5563e77c0be79a1896901e6e
JOSHWIN JAMES <joshwinjames2028@cs.sjcetpalai.ac.in>
2024-10-15 16:03:07 +0530

    python lab 15 10 2024

---
+++ odd  no.py
@@ -0,0 +1,9 @@
+'''Author : Joshwin James
+Date : 15-10-2024
+To print n number of odd numbers'''
+l=int(input("Enter the limit :"))
+no=1
+while l>0:
+    print(no, ' ', end=' ')
+    no+=2
+    l-=1
---
+++ odd no limit.py
@@ -0,0 +1,8 @@
+'''Author : Joshwin James
+Date : 15-10-2024
+To print odd numbers within a limit'''
+l=int(input("Enter the limit :"))
+no=1
+while no<l:
+    print(no, ' ', end=' ')
+    no+=2
