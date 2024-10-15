39c76ff48b800484519797ed6ccd14212e03cc6a
JOSHWIN JAMES <joshwinjames2028@cs.sjcetpalai.ac.in>
2024-10-15 14:08:13 +0530

    probelm of zero fixed

--- integer.py
+++ integer.py
@@ -5,5 +5,7 @@ To check the number is positive or negetive'''
 no=int(input("enter number"))
 if no>0:
     print("The number is positive")
+elif no=0:
+    print("the number is zero")
 else:
     print("The number is negetive")
